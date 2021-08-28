f34bbe476ba3ad55c2de491a6735f260ddf4f4b6
Achmad Fadhitya Muharram <afadhitya@gmail.com>
2021-08-28 11:12:56 +0700

    Add java spring project

---
+++ src/main/java/com/afadhitya/witchdefender/WitchDefenderApplication.java
@@ -0,0 +1,13 @@
+package com.afadhitya.witchdefender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitchDefenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WitchDefenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/afadhitya/witchdefender/WitchDefenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afadhitya.witchdefender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitchDefenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
