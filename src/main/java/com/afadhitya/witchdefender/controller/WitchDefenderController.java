728a8bea7b134b626d53fb651e69064b53330a7a
Achmad Fadhitya Muharram <afadhitya@gmail.com>
2021-08-28 11:36:21 +0700

    Add controller for get average number people killed by watcher

--- src/main/java/com/afadhitya/witchdefender/controller/WitchDefenderController.java
+++ src/main/java/com/afadhitya/witchdefender/controller/WitchDefenderController.java
@@ -1,10 +1,28 @@
 package com.afadhitya.witchdefender.controller;
 
-import org.springframework.web.bind.annotation.Mapping;
+import com.afadhitya.witchdefender.controller.request.DefendWitchRequest;
+import com.afadhitya.witchdefender.controller.response.CommonResponse;
+import com.afadhitya.witchdefender.controller.response.DefendWitchResponse;
+import com.afadhitya.witchdefender.service.WitchDefenderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/witchDefender")
 public class WitchDefenderController {
+
+    private final WitchDefenderService service;
+
+    public WitchDefenderController(WitchDefenderService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/getAverageNumberWitcherKilled")
+    public CommonResponse<DefendWitchResponse> getAverageNumberWitcherKilled(@RequestBody DefendWitchRequest request) {
+        DefendWitchResponse response = service.calculateAverageNumberKilledByWitch(request);
+        return new CommonResponse<>(response, HttpStatus.OK.value());
+    }
 }
