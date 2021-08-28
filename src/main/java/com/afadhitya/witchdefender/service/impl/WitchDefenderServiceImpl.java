5ab2d674a18fd2e1e1fa73856a79c451f91ed93f
Achmad Fadhitya Muharram <afadhitya@gmail.com>
2021-08-28 11:33:31 +0700

    Prepare service class

--- src/main/java/com/afadhitya/witchdefender/service/impl/WitchDefenderServiceImpl.java
+++ src/main/java/com/afadhitya/witchdefender/service/impl/WitchDefenderServiceImpl.java
@@ -1,8 +1,15 @@
 package com.afadhitya.witchdefender.service.impl;
 
+import com.afadhitya.witchdefender.controller.request.DefendWitchRequest;
+import com.afadhitya.witchdefender.controller.response.DefendWitchResponse;
 import com.afadhitya.witchdefender.service.WitchDefenderService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WitchDefenderServiceImpl implements WitchDefenderService {
+
+    @Override
+    public DefendWitchResponse calculateAverageNumberKilledByWitch(DefendWitchRequest request) {
+        return null;
+    }
 }
